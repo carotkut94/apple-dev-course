ecae401c6a4b9a4060a362efcf00b9a0fbd1ae05
carotkut94 <carotkut12@gmail.com>
2022-03-25 02:22:22 +0530

    feat(style-a11y): adds custom label style and accessiblity to card components

---
+++ meetings/TrailingIconLabelStyle.swift
@@ -0,0 +1,24 @@
+//
+//  TrailingIconLableStyle.swift
+//  meetings
+//
+//  Created by Deathcode on 25/03/22.
+//
+
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle{
+    func makeBody(configuration: Configuration) -> some View {
+        HStack{
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+
+extension LabelStyle where Self == TrailingIconLabelStyle{
+    static var trailingIcon: Self {
+        Self()
+    }
+}
