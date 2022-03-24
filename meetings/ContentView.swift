d744537abc29350dff1c7bc7ee607f65af29cde8
carotkut94 <carotkut12@gmail.com>
2022-03-25 01:17:13 +0530

    Initial Commit

---
+++ meetings/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  meetings
+//
+//  Created by Deathcode on 25/03/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ meetings/meetingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  meetingsApp.swift
+//  meetings
+//
+//  Created by Deathcode on 25/03/22.
+//
+
+import SwiftUI
+
+@main
+struct meetingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
