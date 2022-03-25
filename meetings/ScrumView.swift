27d5634d801d39709502eb5d8c3e6dad93b066f3
carotkut94 <carotkut12@gmail.com>
2022-03-26 00:28:41 +0530

    feat(*): adds basic flow and navigation

---
+++ meetings/ScrumView.swift
@@ -0,0 +1,37 @@
+//
+//  ScrumView.swift
+//  meetings
+//
+//  Created by Deathcode on 25/03/22.
+//
+
+import SwiftUI
+
+struct ScrumView: View {
+    let scrums: [DailyScrum]
+    var body: some View {
+        List {
+            ForEach(scrums){ scrum in
+                NavigationLink(destination: DetailView(scrum:scrum)){
+                    CardView(scrum: scrum)
+                }
+                .listRowBackground(scrum.theme.mainColor)
+            }
+        }
+        .navigationTitle("Daily Scrums")
+        .toolbar {
+            Button(action: {}){
+                Image(systemName: "plus")
+            }
+            .accessibilityLabel("New Scrum")
+        }
+    }
+}
+
+struct ScrumView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            ScrumView(scrums: DailyScrum.sampleData)
+        }
+    }
+}
--- meetings/meetingsApp.swift
+++ meetings/meetingsApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct meetingsApp: App {
     var body: some Scene {
         WindowGroup {
-            MeetingView()
+            NavigationView {
+                ScrumView(scrums: DailyScrum.sampleData)
+            }
         }
     }
 }
