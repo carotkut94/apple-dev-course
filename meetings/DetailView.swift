d78303f6b9a6938d4ec346c9ce1fa9680de61510
carotkut94 <carotkut12@gmail.com>
2022-03-26 01:20:54 +0530

    feat(editing): ads editing view for meeeting

--- meetings/DetailView.swift
+++ meetings/DetailView.swift
@@ -11,6 +11,8 @@ struct DetailView: View {
     
     let scrum: DailyScrum
     
+    @State private var isPresentingEditView = false
+    
     var body: some View {
         List {
             Section(header: Text("Meeting Info")){
@@ -45,6 +47,27 @@ struct DetailView: View {
             }
         }
         .navigationTitle(scrum.title)
+        .toolbar{
+            Button(action: {
+                isPresentingEditView = true
+            }){
+                Image(systemName: "pencil")
+                    .accessibilityLabel("Edit Meeting")
+            }
+        }
+        .sheet(isPresented: $isPresentingEditView){
+            NavigationView {
+                DetailEditView()
+                    .navigationTitle(scrum.title)
+                    .toolbar {
+                        ToolbarItem(placement: .cancellationAction){
+                            Button("Cancel"){
+                                isPresentingEditView = false
+                            }
+                        }
+                    }
+            }
+        }
     }
 }
 
